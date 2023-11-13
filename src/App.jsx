2b57f363c5112676d6545be17915b80f18da3cc7
Monica <mmunzp@gmail.com>
2023-11-13 10:03:04 +0100

    N1. Exercici 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import Card from "./Card.jsx";
 import { useState } from "react";
+import styled from "styled-components";
+import { createGlobalStyle } from "styled-components";
 
 import image1 from "./assets/time_managment.svg";
 import image2 from "./assets/programming.svg";
@@ -11,21 +13,21 @@ export function App() {
       title: "Dedica moltes hores",
       description:
         "Un mínim de 30 hores. Si no en tens prou, hauràs de dedicar-li més hores. Al principi sembla impossible, però notaràs una millora ràpidament.",
-      bgColor: "blue",
+      bgColor: "#01A9DB",
       image: image1,
     },
     {
       title: "Programa projectes propis",
       description:
         "Més val 10 hores treballant en projectes propis, que 10 hores mirant tutorials. La motivació i la implicació en el projecte ajudarà a accelerar el teu aprenentatge.",
-      bgColor: "gray",
+      bgColor: "#BDBDBD",
       image: image2,
     },
     {
       title: "Procura descansar",
       description:
         "Descansar bé i desconnectar són vitals. D'aquesta manera reduiràs l'estrès i l'ansietat. Milloraràs la teva concentració i consolidaràs el teu aprenentatge.",
-      bgColor: "yellow",
+      bgColor: "#FF8000",
       image: image3,
     },
   ];
@@ -34,11 +36,30 @@ export function App() {
   const currentCardData = tutorialData[step];
 
   function nextStep() {
-    setStep(prev => prev + 1);
+    setStep((prev) => prev + 1);
   }
+
+  const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+`;
+  const AppStyle = styled.div`
+    background-color: #f2f2f2;
+    height: 100vh;
+    margin: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `;
+
   return (
-    <div className="App">
-      <Card currentCardData={currentCardData} nextStep={nextStep}/>
-    </div>
+    <>
+      <GlobalStyle />
+      <AppStyle className="App">
+        <Card currentCardData={currentCardData} nextStep={nextStep} />
+      </AppStyle>
+    </>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,13 +1,69 @@
+import styled from "styled-components";
+
 function Card({ currentCardData, nextStep }) {
-    return (
-      <div className="card">
-        <div className="card-body">
-          <h1 className="card-title">{currentCardData.title}</h1>
-          <p className="card-content">{currentCardData.description}</p>
-          <button onClick={nextStep}>→</button>
-        </div>
-      </div>
-    );
-  }
-  
-  export default Card;
+  const CardContainer = styled.div`
+    margin: 0;
+    font-family: "Poppins", sans-serif;
+    background-color: ${(props) => props.bgColor || "white"};
+    width: 350px;
+    height: 700px;
+
+    box-shadow: 2px 2px 40px gray;
+    border-radius: 30px;
+  `;
+
+  const ImageContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 30px;
+    width: 100%;
+    height: 60%;
+    img {
+      width: 80%;
+      height: 80%;
+    }
+  `;
+  const CardBody = styled.div`
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    background-color: white;
+    padding: 20px 25px;
+    border-radius: 0 0 30px 30px;
+  `;
+  const CardTitle = styled.h1`
+    font-size: 20px;
+    font-weight: bold;
+  `;
+  const CardP = styled.p`
+    font-size: 14px;
+    margin: 0;
+  `;
+
+  const Button = styled.button`
+    background-color: #2e2e2e;
+    color: white;
+    border-radius: 50%;
+    padding: 20px;
+    margin: 25px 15px;
+    border: none;
+    cursor: pointer;
+    align-self: flex-end;
+  `;
+
+  return (
+    <CardContainer bgColor={currentCardData.bgColor}>
+      <ImageContainer>
+        <img src={currentCardData.image} />
+      </ImageContainer>
+      <CardBody>
+        <CardTitle className="card-title">{currentCardData.title}</CardTitle>
+        <CardP className="card-content">{currentCardData.description}</CardP>
+        <Button onClick={nextStep}>→</Button>
+      </CardBody>
+    </CardContainer>
+  );
+}
+
+export default Card;
